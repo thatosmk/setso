f97ed53b70b6434c5dcdf9f5aa96a40fa44bcb86
Thato Semoko <thatosmk@gmail.com>
2020-10-17 08:20:19 +0200

    adds styling for root page and login pages

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,5 @@
 class SiteController < ApplicationController
   def index
+    redirect_to recipes_path if user_signed_in?
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
     end
     doc.to_html.html_safe
   end
+
+  def title text
+    content_for(:title) { text }
+  end
+
+  def meta_tag tags, text
+    content_for :"meta_#{tags}", text 
+  end
+
+  def yield_meta_tag tags, default_text='' 
+    content_for?(:"meta_#{tags}") ? content_for(:"meta_#{tags}") : default_text
+  end
 end
